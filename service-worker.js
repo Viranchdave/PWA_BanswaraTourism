ae21fc64e541811c4d48899de5cf821531c0e5f2
Viranchdave <76239614+Viranchdave@users.noreply.github.com>
2025-06-05 10:13:17 +0530

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,29 @@
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open('safar-cache-v1').then(cache => {
+      return cache.addAll([
+        'index (38).html',
+        'SAFAR (10).png',
+        'ChatGPT Image May 21, 2025, 10_07_56 PM.png',
+        'Home.html',
+        'About Banswara.html',
+        'NearestAttract.html',
+        'TOC5 - Copy (4).html',
+        'TouristCircuits.html',
+        '21.html',
+        'Cont1.html',
+        'privacy_policy_embedded.html',
+        'C.html'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
